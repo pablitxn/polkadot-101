c19714c059b7995bb04b824faa59cef206ecb1c2
Pablo Coronel <me@pablitxn.io>
2022-07-30 16:50:46 -0300

    initial commit

---
+++ blockchain-node/runtime/src/custom-module.rs
@@ -0,0 +1,15 @@
+// use support::(del_storage, decl_module);
+
+// pub trait Trait: system::Trait {}
+
+// decl_storage! {
+//   trait Store for Module<T: Trait> as CustomModuleStorage {
+//     // se declaran aqui los datos del modulo y funciones read-only
+//   }
+// }
+
+// decl_module! {
+//   pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+//     // se declaran acá las funciones especiales que cambian el estado
+//   }
+// }
